c97485f3f1bced7eb5b936259afa796786fe816d
Mohamad-Hachem <mohamad27hachem@outlook.com>
2024-11-18 16:09:25 +0100

    the new files

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,46 @@
 import { createStore } from 'vuex'
+import {FALSE} from "sass";
+import {toKey} from "core-js/internals/reflect-metadata";
 
 export default createStore({
   state: {
+    isLoading : false,
+    isAuthenticated: false,
+    token: ''
   },
   getters: {
   },
   mutations: {
+    initializeStore(state)
+    {
+      if(localStorage.getItem('token'))
+      {
+        state.token = localStorage.getItem('token');
+        state.isAuthenticated = true;
+      }
+      else
+      {
+        state.token = '';
+        state.isAuthenticated = false;
+      }
+    },
+    setIsLoading(state, status)
+    {
+      state.isLoading = status;
+    },
+
+    setToken(state, token)
+    {
+      state.token = token;
+      state.isAuthenticated = true;
+    },
+
+    removeToken(state)
+    {
+      state.token = '';
+      state.isAuthenticated = false ;
+    }
+
   },
   actions: {
   },
---
+++ src/views/SignUp.vue
@@ -0,0 +1,105 @@
+<template>
+  <div class="container">
+    <div class="columns">
+      <div class="column is-4 is-offset-4">
+        <h1 class="title"> Sign up</h1>
+        <form @submit.prevent="submitForm">
+          <div class="field">
+            <label>Email</label>
+            <div class="control">
+              <input type="email" name="email" class="input" v-model="username">
+            </div>
+          </div>
+
+          <div class="field">
+            <label>Password</label>
+            <div class="control">
+              <input type="password" name="password1" class="input" v-model="password1">
+            </div>
+          </div>
+
+          <div class="field">
+            <label>Repeat Password</label>
+            <div class="control">
+              <input type="password" name="password2" class="input" v-model="password2">
+            </div>
+          </div>
+
+          <div class="notification is-danger" v-if="errors.length">
+            <p v-for="error in errors" :key="error">
+              {{error}}
+            </p>
+          </div>
+
+          <div class="field">
+            <div class="control">
+              <button class="button is-success">Submit</button>
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+</template>
+<script >
+  import axios from "axios";
+  import {toast} from "bulma-toast";
+
+  export default {
+    name: 'SignUp',
+    data() {
+      return {
+        username: '',
+        password1: '',
+        password2: '',
+        errors: [],
+      }
+    },
+    methods:
+        {
+          async submitForm(){
+            this.errors = [];
+            if(this.username === ''){
+              this.errors.push("the username cannot be empty")
+            }
+            if(this.password1 === ''){
+              this.errors.push("the password1 cannot be empty")
+            }
+            if(this.password1 !== this.password2){
+              this.errors.push("The passwords are not matching!")
+            }
+            if(!this.errors.length){
+              this.$store.commit('setIsLoading', true)
+
+              const formData = {
+                username: this.username,
+                password: this.password1,
+              }
+              await axios.post('/api/v1/users/', formData).then(response =>
+                  {
+                    toast({
+                      message: 'Account was created, please log in',
+                      type: 'is-success',
+                      dismissible: true,
+                      pauseOnHover: true,
+                      duration: 2000,
+                      position: 'bottom-right',
+                    })
+
+                    this.$router.push('/log-in')
+                  }
+              ).catch(error => {
+                if(error.response) {
+                    this.errors.push(error.response.data)
+                }
+                else if (error.message)
+                {
+                  this.errors.push('Something went wrong(in SignUp) please Try again!')
+                }
+              })
+              this.$store.commit('setIsLoading', false)
+            }
+          },
+        }
+  }
+</script>
